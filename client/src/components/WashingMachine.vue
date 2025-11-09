d4b458f04b907315cd945084707aee7f3a57bb59
BananaCotton57 <johnnyngo772@gmail.com>
2025-11-08 23:29:43 -0500

    added dummy javascript to the washingmachine.vue component

--- client/src/components/WashingMachine.vue
+++ client/src/components/WashingMachine.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 import washingMachine from '../assets/washing-machine.png'
+
+const status = ref('Idle') //this is dummy javascript. You can remove it
 </script>
 
 <template>
     <div>
         <img class="washingMachine":src="washingMachine" alt="Washing Machine" />
-        <h1 class="status">Status: </h1> <!--Change this if you want-->
+        <h1 class="status">Status: {{ status }}</h1> <!--Change this if you want-->
+        <button @click="status = status === 'Idle' ? 'Running' : 'Idle'">Toggle</button> <!--this is also dummy javascript that you can remove-->
     </div>
 </template>
 
