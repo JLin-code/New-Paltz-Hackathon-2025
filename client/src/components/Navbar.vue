9eb1d4a62cb849f8bb5ca78e0d043164780a8d04
BananaCotton57 <johnnyngo772@gmail.com>
2025-11-08 20:14:10 -0500

    setting up the web page. made a navbar to switch between halls and made some components

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Bouton Hall
+      </a>
+
+      <a class="navbar-item">
+        Awoosting Hall
+      </a>
+
+      <a class="navbar-item">
+        Ridgeview Hall
+      </a>
+
+      <a class="navbar-item">
+        Ridgeview Hall
+      </a>
+
+    </div>
+
+  </div>
+</nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
