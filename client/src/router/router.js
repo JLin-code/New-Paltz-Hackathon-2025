c7fdc8590c09f2402f14b80090887e39f85c006c
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-11-09 00:36:31 -0500

    updated router

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -3,33 +3,8 @@ import { routes, handleHotUpdate } from "vue-router/auto-routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../pages/index.vue'),
-    },
-    {
-      path: '/BoutonHall',
-      name: 'BoutonHall',
-      component: () => import('../pages/BoutonHall.vue'),
-    },
-    {
-      path: '/AwoostingHall',
-      name: 'AwoostingHall',
-      component: () => import('../pages/AwoostingHall.vue'),
-    },
-    {
-      path: '/RidgeviewHall',
-      name: 'RidgeviewHall',
-      component: () => import('../pages/RidgeviewHall.vue'),
-    },
-    {
-      path: '/EsopusHall',
-      name: 'EsopusHall',
-      component: () => import('../pages/EsopusHall.vue'),
-    },
-  ]
+  routes,
+  // vue-router gives file-based routes
 });
 
 if (import.meta.hot) {
