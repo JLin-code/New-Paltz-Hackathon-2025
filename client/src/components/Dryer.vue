fd007d3317552dd645a4f4fdf474190c9c12bdc8
BananaCotton57 <johnnyngo772@gmail.com>
2025-11-09 09:45:13 -0500

    added dummy functionality to dryers

--- client/src/components/Dryer.vue
+++ client/src/components/Dryer.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 import dryer from '../assets/tumble-dryer.png'
+
+const status = ref('Idle') //this is dummy javascript. You can remove it
 </script>
 
 <template>
     <div>
         <img class="dryer":src="dryer" alt="Dryer" />
-        <h1 class="status">Status: </h1> <!--Change this if you want-->
+        <h1 class="status">Status: {{ status }}</h1> <!--Change this if you want-->
+        <button @click="status = status === 'Idle' ? 'Running' : 'Idle'">Toggle</button> <!--this is also dummy javascript that you can remove-->
     </div>
 </template>
 
